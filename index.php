387840c409b86d7452560dd6661bd82184686981
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-05-23 14:41:13 +0200

    conflit sur index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-
-require_once("./php/Classes/ProductsOptions.php");
-use Classes\ProductsOptions;
-
-$option = new ProductsOptions();
-$option->deleteRow(3);
-var_dump($option->getAll()->fetchAll(PDO::FETCH_ASSOC));
-$option->updateQuantity(3,1);
-$option->updateValue("vert",1);
-$option->updatePrice(7,1);
-echo $option->findIdWith("vert", "name");
-echo "<br>";
-
+require('./Templates/index.html');
 
