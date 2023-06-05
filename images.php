85043502e8df9889340927e526a8f0fd5fc5e34e
miguel-molia <miguel.molia@laplateforme.io>
2023-06-05 12:09:33 +0200

    correction images

---
+++ images.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-28 w-52 rounded-lg" src="./Public/img/Pull/pull_femme_noir.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-28 w-52 rounded-lg" src="./Public/img/Pull/pull_femme_couleur.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-28 w-52 rounded-lg" src="./Public/img/Short/short_jean_homme.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-28 w-52 rounded-lg" src="./Public/img/Short/short_jean_femme.png" alt="">
+    </div>
+    <div>
+        <img class="h-28 w-52 rounded-lg" src="./Public/img/T-shirt/t_shirt_blanc.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-28 w-52 rounded-lg" src="./Public/img/T-shirt/t_shirt_noir.jpg" alt="">
+    </div>
+  
