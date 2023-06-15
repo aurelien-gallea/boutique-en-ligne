30563fc957dd92c05805ebc5f9048f85a3cc85f4
miguel-molia <miguel.molia@laplateforme.io>
2023-06-15 13:22:43 +0200

    ajout des comment

--- assets/js/admin/allProducts.js
+++ assets/js/admin/allProducts.js
@@ -1,53 +1,69 @@
+// Importer la fonction addElement du fichier ../modules/addElement.js
 import { addElement } from "../modules/addElement.js";
+
+// Attendre que le contenu soit chargé avant d'exécuter le code
 document.addEventListener("DOMContentLoaded", function() {
 
-fetch('../../php/Json/AllProducts.php')
-    .then(response => response.json())
-    .then( products => {
-        
-        if(products && products.length){
+    // Récupérer les produits à partir du fichier JSON
+    fetch('../../php/Json/AllProducts.php')
+        .then(response => response.json())
+        .then(products => {
 
-            let ContainerTable = addElement('div', ["flex", "flex-col", "w-full", "shadow-md", "bg-white", "rounded-lg", "p-4", "space-y-3", "dark:bg-gray-800", "dark:border"], {});
-            document.getElementById('prodContainer').appendChild(ContainerTable);
+            // Vérifier s'il y a des produits et qu'ils ont une longueur
+            if (products && products.length) {
 
-            let tableProd = addElement('table', ["w-full", "text-sm", "text-left", "text-gray-500", "dark:text-gray-400"], {});
-            ContainerTable.appendChild(tableProd);
+                // Créer un élément div avec des classes et des attributs spécifiques et l'ajouter au conteneur prodContainer
+                let ContainerTable = addElement('div', ["flex", "flex-col", "w-full", "shadow-md", "bg-white", "rounded-lg", "p-4", "space-y-3", "dark:bg-gray-800", "dark:border"], {});
+                document.getElementById('prodContainer').appendChild(ContainerTable);
 
-            let thead = addElement('thead', ["text-xs", "text-gray-700", "uppercase", "bg-gray-50", "dark:bg-gray-700", "dark:text-gray-400"], {});
-            tableProd.appendChild(thead);
+                // Créer un élément table avec des classes et des attributs spécifiques et l'ajouter au ContainerTable
+                let tableProd = addElement('table', ["w-full", "text-sm", "text-left", "text-gray-500", "dark:text-gray-400"], {});
+                ContainerTable.appendChild(tableProd);
 
-            let trthead = addElement('tr', [], {});
-            thead.appendChild(trthead);
-            
-            let thId = addElement('th', ["px-6", "py-3"], {scope: "col"}, "Id");
-            let thName = addElement('th', ["px-6", "py-3"], {scope: "col"}, "Nom");
-            let thDate = addElement('th', ["px-6", "py-3"], {scope: "col"}, "créé le"); 
-            trthead.appendChild(thId);
-            trthead.appendChild(thName);
-            trthead.appendChild(thDate);
+                // Créer un élément thead avec des classes et des attributs spécifiques et l'ajouter au tableProd
+                let thead = addElement('thead', ["text-xs", "text-gray-700", "uppercase", "bg-gray-50", "dark:bg-gray-700", "dark:text-gray-400"], {});
+                tableProd.appendChild(thead);
 
-            let tbody = addElement('tbody', [], {});
-            thead.after(tbody);
+                // Créer un élément tr pour l'en-tête de la table et l'ajouter au thead
+                let trthead = addElement('tr', [], {});
+                thead.appendChild(trthead);
 
-            products.map(item => {
-        
-                let trtbody = addElement('tr', ["bg-white", "border-b", "dark:bg-gray-800", "dark:border-gray-700"], {});
-                tbody.appendChild(trtbody);
+                // Créer les éléments th pour chaque colonne de l'en-tête (Id, Nom, créé le) et les ajouter au trthead
+                let thId = addElement('th', ["px-6", "py-3"], { scope: "col" }, "Id");
+                let thName = addElement('th', ["px-6", "py-3"], { scope: "col" }, "Nom");
+                let thDate = addElement('th', ["px-6", "py-3"], { scope: "col" }, "créé le");
+                trthead.appendChild(thId);
+                trthead.appendChild(thName);
+                trthead.appendChild(thDate);
 
-                let thContentId = addElement('th', ["px-6", "py-4", "font-normal", "text-gray-900", "whitespace-nowrap", "dark:text-white"], {scope: "row"}, `${item.id}`);
-                let thContentName = addElement('td', ["px-6", "py-4"], {}, `${item.name}`);
-                trtbody.appendChild(thContentId);
-                trtbody.appendChild(thContentName);
-                
-                let date = new Date(item.createdAt);
-                let jour = date.getDate();
-                let mois = date.getMonth() + 1;
-                let annee = date.getFullYear();
-                let dateFormat = `${jour.toString().padStart(2, '0')}-${mois.toString().padStart(2, '0')}-${annee}`;
-                let thContentDate = addElement('td', ["px-6", "py-4"], {}, `${dateFormat}`);
-                trtbody.appendChild(thContentDate);
-            })
-        }
-    });
+                // Créer un élément tbody et l'ajouter après le thead
+                let tbody = addElement('tbody', [], {});
+                thead.after(tbody);
 
+                // Parcourir les produits et créer les éléments correspondants pour chaque produit
+                products.map(item => {
+
+                    // Créer un élément tr pour chaque ligne du corps de la table et l'ajouter au tbody
+                    let trtbody = addElement('tr', ["bg-white", "border-b", "dark:bg-gray-800", "dark:border-gray-700"], {});
+                    tbody.appendChild(trtbody);
+
+                    // Créer les éléments th pour chaque contenu (Id, Nom) et les ajouter au trtbody
+                    let thContentId = addElement('th', ["px-6", "py-4", "font-normal", "text-gray-900", "whitespace-nowrap", "dark:text-white"], { scope: "row" }, `${item.id}`);
+                    let thContentName = addElement('td', ["px-6", "py-4"], {}, `${item.name}`);
+                    trtbody.appendChild(thContentId);
+                    trtbody.appendChild(thContentName);
+
+                    // Formater la date dans un format spécifique (jour-mois-année)
+                    let date = new Date(item.createdAt);
+                    let jour = date.getDate();
+                    let mois = date.getMonth() + 1;
+                    let annee = date.getFullYear();
+                    let dateFormat = `${jour.toString().padStart(2, '0')}-${mois.toString().padStart(2, '0')}-${annee}`;
+
+                    // Créer l'élément th pour la date et l'ajouter au trtbody
+                    let thContentDate = addElement('td', ["px-6", "py-4"], {}, `${dateFormat}`);
+                    trtbody.appendChild(thContentDate);
+                })
+            }
+        });
 });
--- signUp.php
+++ signUp.php
@@ -19,7 +19,7 @@ if (isset($_POST['signUp'])) {
             $pass1 = htmlspecialchars($_POST['password']);
             $pass2 = htmlspecialchars($_POST['confirm-password']);
 
-            
+
             // verifications du mail (au cas où l'utilisateur change le type de l'input)
             if (!Verify::verifySyntax($email)) {
                 header('location:signUp.php?error=1&message=merci de rentrer un email valide !');
@@ -59,15 +59,18 @@ ob_start();
                     Créer un compte
                 </h1>
                 <form class="space-y-4 md:space-y-6" method="post" action="">
-                <div>
-                        <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre prénom</label>
-                        <input type="text" name="firstname" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                    <div class="flex gap-3">
+
+                        <div>
+                            <label for="firstname" class=" block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre prénom</label>
+                            <input type="text" name="firstname" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                        </div>
+                        <div>
+                            <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre nom</label>
+                            <input type="text" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                        </div>
                     </div>
-                <div>
-                        <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre nom</label>
-                        <input type="text" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                    </div>    
-                <div>
+                    <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre adresse e-mail</label>
                         <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
                     </div>
