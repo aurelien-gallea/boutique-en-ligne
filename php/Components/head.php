4845cccbdb0be28ca7864b743a1373dde990f38c
hugocanovas <canovashugo83@gmail.com>
2023-06-22 01:13:31 +0200

    mise en page des produits

--- php/Components/head.php
+++ php/Components/head.php
@@ -9,8 +9,6 @@ if(isset($_SESSION['id']));
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>    
-    <link href="./public/assets/images/favicon.ico" rel="icon" type="image/x-icon" >
-    <link rel="stylesheet" href="./public/design/default.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
